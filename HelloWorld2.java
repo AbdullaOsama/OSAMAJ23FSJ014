8d858255090d5cea8d4e59e4c32608d99d881570
Osama Abdulla <115803621+AbdullaOsama@users.noreply.github.com>
2023-07-03 19:52:33 +0530

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,15 @@
+package eve;
+
+import java.util.Scanner;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+System.out.println("Please Enter Your Name...:");
+String name=scan.next();
+
+System.out.println("Welcome To Java...:"+name);
+	}
+
+}
--- OperatorsDemo.java
+++ OperatorsDemo.java
@@ -1,64 +1,90 @@
-package Mod1Day7;
+package eve;
 
 public class OperatorsDemo {
-	
-     public static void main(String[] args) {
-	
-    	 byte i=11;
- 		byte j=2;
- 		
- 		System.out.println("Addition ...:"+(i+j));
- 		System.out.println("Substraction ...:"+(i-j));
- 		System.out.println("Multiplication ...:"+(i*j));
- 		System.out.println("Division ..**QUOTIENT**.:"+(i/j));
- 		System.out.println("Division..**Reminder**"+(i%j));//% symbol is called MOD
- 		
- 		//++ this is equivalent to x=x+1;
- 		int x=10;
- 		//x=x+1;
- 		System.out.println(x++);//here the value is first printed then incremented - post increment
- 		System.out.println(x);
- 		
- 		x=10;
- 		System.out.println(++x);//here the value is first incremented then printed - pre increment
- 		System.out.println(x);
- 		
- 		x=10;
- 		System.out.println(x--);//here the value is first printed then decremented - post decrement
- 		System.out.println(x);
- 		
- 		x=10;
- 		System.out.println(--x);//here the value is first decremented then printed - pre decrement
- 		System.out.println(x);
- 		
- 		//x=x+2; x=x+5;
- 		
- 		x=10;
- 		//x=x+5;
- 		x+=5;
- 		System.out.println(x);
- 		
- 		x=10;
- 		//x=x*5;
- 		x*=5;
- 		System.out.println(x);
- 		
- 		x=10;
- 		//x=x-5;
- 		x-=5;
- 		System.out.println(x);
- 		
- 		x=10;
- 		//x=x/5;
- 		x/=5;//this will give you the quotient
- 		System.out.println(x);
- 		
- 		x=11;
- 		//x=x%2;//this prin
- 		x=x%2;//this will give the reminder
- 		System.out.println(x);
- 		
- 		
- 		
- 	}
- }
+
+	public static void main(String[] args) {
+		int i=11;
+		int j=2;
+		
+		System.out.println("Addition..:"+(i+j));
+		System.out.println("Subtraction..:"+(i-j));
+		System.out.println("Multiplication..:"+(i*j));
+		System.out.println("Division ..*Quotient*..:"+(i/j));
+		System.out.println("Division..*Reminder*..:"+(i%j));
+		
+		int x=10;
+		//increment x by 1
+		x=x+1;
+		System.out.println(x);
+		
+		x=10;
+		x=x-1;
+		System.out.println(x);
+		
+		x=10;
+		System.out.println(x++);//x++ is equivalent to x=x+1
+		
+		//x++ is post increment and ++x is pre increment
+		//x++ - prints the x first and then increments
+		
+		System.out.println(x);
+		
+		x=10;
+		System.out.println(++x);
+		
+		x=10;
+		System.out.println(x--);//prints first and then decrements
+		System.out.println(x);
+		
+		x=10;
+		System.out.println(--x);//decrement first and then print
+		
+		//can i increment to more than 1
+		x=10;
+		x=x+5;
+		System.out.println(x);
+		
+		//this also has short cut
+		
+		x=10;
+		x+=5;//this is equivalent to x=x+5;
+		System.out.println(x);
+		
+		x=10;
+		x-=5;//this is equivalent to x=x-5;
+		System.out.println(x);
+		
+		x=10;
+		x*=5;//this is equivalent to x=x*5;
+		System.out.println(x);
+		
+		x=10;
+		x/=5;//this is equivalent to x=x/5;
+		System.out.println(x);
+		
+		x=10;
+		x%=5;//this is equivalent to x=x%5;
+		System.out.println(x);
+		
+		//RELATIONAL OPERATORS - will always return true or false
+		//== (COMPARISON)
+		i=10;
+		j=2;
+		System.out.println(i==j);//will return false because i and j are not equal
+		System.out.println(i!=j);//will return true because i and j are not equal
+		System.out.println(i<j);//i is lesser than j if so then true
+		System.out.println(i>j);//i is greater than j if so then true
+		System.out.println(i<=j);//i is lesser than or equal to j then true
+		System.out.println(i>=j);//i is greater than or equal to j then true
+		
+		//LOGICAL OPERATORS - &&(AND) AND ||(OR)
+		
+		System.out.println(i>j && i==100);//ampersand symbol
+		//if both are true, then the return will be true
+		
+		System.out.println(i>j || i==100);//pipe symbol
+		//even if one is true, then the return will true
+
+	}
+
+}
